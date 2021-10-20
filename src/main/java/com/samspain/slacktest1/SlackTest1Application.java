3547c5ec97d4bdcfb97b9a5207000b3cd41cf301
sam <sam@samspain.com>
2021-10-20 17:09:42 +0100

    Initial commit

---
+++ src/main/java/com/samspain/slacktest1/SlackTest1Application.java
@@ -0,0 +1,13 @@
+package com.samspain.slacktest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/samspain/slacktest1/SlackTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samspain.slacktest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
